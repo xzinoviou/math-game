3b23577677015655b5b38fc78629725c29b8a1ae
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-10-23 19:10:37 +0200

    gamification: added project

---
+++ gamification/src/test/java/com/xzinoviou/gamification/GamificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzinoviou.gamification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamificationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
