ae3c21dbd799016ab2f9d910fb50d5e27b8ecbd4
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-10-23 19:58:18 +0200

    gamification: add ScoreCard

---
+++ gamification/src/main/java/com/xzinoviou/gamification/domain/ScoreCard.java
@@ -0,0 +1,54 @@
+package com.xzinoviou.gamification.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * Binds score with a multiplication attempt.
+ *
+ * @author : Xenofon Zinoviou
+ */
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+@Entity
+public class ScoreCard {
+
+  @Value("${score.card.default.score}")
+  private static int DEFAULT_SCORE;
+
+  @Id
+  @GeneratedValue
+  @Column(name = "CARD_ID")
+  private final Long id;
+
+  @Column(name = "USER_ID")
+  private final Long userId;
+
+  @Column(name = "ATTEMPT_ID")
+  private final Long attemptId;
+
+  @Column(name = "SCORE_TS")
+  private final long badgeTimestamp;
+
+  @Column(name = "SCORE")
+  private final int score;
+
+  //Empty constructor for JSON / JPA
+  public ScoreCard() {
+    this(null, null, null, 0, 0);
+  }
+
+  public ScoreCard(Long userId, Long attemptId) {
+    this(null, userId, attemptId, System.currentTimeMillis(), DEFAULT_SCORE);
+
+  }
+}
