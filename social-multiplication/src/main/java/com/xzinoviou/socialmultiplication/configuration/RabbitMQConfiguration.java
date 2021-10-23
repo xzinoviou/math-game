0c56175f0b9a97b54c53e1a23354df547d05fad0
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-10-23 17:52:54 +0200

    social-multiplication: added RabbitMQConfiguration class

---
+++ social-multiplication/src/main/java/com/xzinoviou/socialmultiplication/configuration/RabbitMQConfiguration.java
@@ -0,0 +1,36 @@
+package com.xzinoviou.socialmultiplication.configuration;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * RabbitMQ Configuration for user of events in the application.
+ *
+ * @author : Xenofon Zinoviou
+ */
+@Configuration
+public class RabbitMQConfiguration {
+
+  @Bean
+  public TopicExchange multiplicationExchange(
+      @Value("${multiplication.exchange}") final String exchangeName) {
+    return new TopicExchange(exchangeName);
+  }
+
+  @Bean
+  public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+    final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+    return rabbitTemplate;
+  }
+
+  @Bean
+  public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
+}
