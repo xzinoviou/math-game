e96d5ef8aeea453610d10c29769dffe8ef161750
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-10-23 19:39:56 +0200

    gamification: add Badge enum

---
+++ gamification/src/main/java/com/xzinoviou/gamification/domain/Badge.java
@@ -0,0 +1,19 @@
+package com.xzinoviou.gamification.domain;
+
+/**
+ * Enumeration of all badges.
+ *
+ * @author : Xenofon Zinoviou
+ */
+public enum Badge {
+
+  //score badges
+  BRONZE_MULTIPLICATION,
+  SILVER_MULTIPLICATION,
+  GOLD_MULTIPLICATION,
+
+  //various badges
+  FIRST_ATTEMPT,
+  FIRST_WON
+
+}
