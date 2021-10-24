b3ca4afbb4ba9d6db3358c0b7181df1992600fe3
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-10-24 10:59:28 +0200

    gamification:Badge-add lucky number value

--- gamification/src/main/java/com/xzinoviou/gamification/domain/Badge.java
+++ gamification/src/main/java/com/xzinoviou/gamification/domain/Badge.java
@@ -8,12 +8,19 @@ package com.xzinoviou.gamification.domain;
 public enum Badge {
 
   //score badges
+
+  //Bronze (100 points or more)
   BRONZE_MULTIPLICATION,
+
+  //Silver (500 points or more)
   SILVER_MULTIPLICATION,
+
+  //Gold (999 points or more)
   GOLD_MULTIPLICATION,
 
   //various badges
   FIRST_ATTEMPT,
-  FIRST_WON
+  FIRST_WON,
+  LUCKY_NUMBER
 
 }
