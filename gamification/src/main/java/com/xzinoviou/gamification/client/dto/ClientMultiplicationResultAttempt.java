a650dd563af028a9c082393da1b59354c3cae34c
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-10-24 11:05:00 +0200

    gamification: add client packjage with ClientMultiplicationResultAttempt

---
+++ gamification/src/main/java/com/xzinoviou/gamification/client/dto/ClientMultiplicationResultAttempt.java
@@ -0,0 +1,31 @@
+package com.xzinoviou.gamification.client.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author : Xenofon Zinoviou
+ */
+@Getter
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
+public class ClientMultiplicationResultAttempt {
+
+  private final String userAlias;
+  private final int multiplicationFactorA;
+  private final int multiplicationFactorB;
+  private final int resultAttempt;
+  private final boolean correct;
+
+  //Empty constructor for JSON (de)serialization
+  public ClientMultiplicationResultAttempt() {
+    userAlias = null;
+    multiplicationFactorA = -1;
+    multiplicationFactorB = -1;
+    resultAttempt = -1;
+    correct = false;
+  }
+}
