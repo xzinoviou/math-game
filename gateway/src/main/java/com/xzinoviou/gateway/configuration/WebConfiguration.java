1744e84167da12639a6cc058e30ca6fc790c9dfc
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-10-31 11:53:07 +0100

    gateway: add WebConfiguration

---
+++ gateway/src/main/java/com/xzinoviou/gateway/configuration/WebConfiguration.java
@@ -0,0 +1,19 @@
+package com.xzinoviou.gateway.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author : Xenofon Zinoviou
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfiguration implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
+  }
+}
